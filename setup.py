949bff58f28da424c5b80619e7035fbe53df5704
Michal Franczel <michal@deepnote.com>
2025-05-19 15:24:46 +0200

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ history = open('CHANGES.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='sqlalchemy-redshift',
-    version='0.8.16.dev3',
+    version='0.8.16.dev4',
     description='Amazon Redshift Dialect for sqlalchemy',
     long_description=readme + '\n\n' + history,
     long_description_content_type='text/x-rst',
